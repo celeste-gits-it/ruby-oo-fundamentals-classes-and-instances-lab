4a3db7f251bb590f5763bea92d3e0897fcf5639a
github username <github email address>
2021-02-23 14:46:12 -0600

    commit tree eb46a369d7850660100f2cc3ca7a4063a6fd2a71

---
+++ lib/dog.rb
@@ -0,0 +1,34 @@
+class Dog
+end
+
+snoopy = Dog.new
+lassie = Dog.new
+fido = Dog.new
+
+
+
+
+
+
+
+
+
+# Classes And Instances Lab Ruby
+
+## Objectives
+
+# 1. Define new Ruby classes with the `class` keyword.
+# 2. Instantiate instances of a `class`.
+
+# ## Overview
+
+# This lab is all about defining classes and instantiating instances.
+
+# ### 1. Define `Dog` in `lib/dog.rb`
+
+# Open `lib/dog.rb` and add a class definition for a `Dog` class.
+
+# ### 2. Make 3 dogs in `lib/dog.rb`
+
+# Under your `Dog` class definition, create three dogs in local variables, `fido`, `snoopy`, and `lassie`.
+
